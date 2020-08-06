6a30e407391f3e0214c8e6cb7253e5ab3844da5b
BarS134 <62279278+BarS134@users.noreply.github.com>
2020-08-06 23:19:20 +0700

    ham toan hoc

---
+++ Baitap1.js
@@ -0,0 +1,3 @@
+//  Math.random() lay ngau nhie:
+// Math.round() Lam tron ceil len floor xuong
+
--- Test.js
+++ Test.js
@@ -1,15 +1,41 @@
-const a = 10;
-const b= 5;
+// const a = 10;
+// const b= 5;
 
 
-if(a>b){
-    console.log(a);
-}
-else{
-    console.log(b)}
+// if(a>b){
+//     console.log(a);
+// }
+// else{
+//     console.log(b)}
 
-    a>b ? console.log(a) : console.log(b);
+//     a>b ? console.log(a) : console.log(b);
 
-max =  a>b ? a : b;
-console.log(max); 
+// max =  a>b ? a : b;
+// console.log(max); 
+/*
+// vong lap for
 
+    // c1
+    for(i = 0; i <100;i++){
+        console.log(i);
+    }
+
+    // c2 
+    for(i = 1;true;i++){
+        if(i>100){
+            break;
+        }
+        console.log(i);
+    }
+    */
+// Vong lap wwhile va do while
+//  i = 1;   
+// while( i< 100){console.log(i++)}
+
+// do while
+//   i =1;
+// do{
+  
+//     console.log(i++);
+// }
+// while(i<100) 
