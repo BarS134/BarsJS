72a144f5be6f76b493b768b36b3eeb22e0042d1a
BarS134 <62279278+BarS134@users.noreply.github.com>
2020-08-06 23:46:09 +0700

    random

--- Baitap1.js
+++ Baitap1.js
@@ -1,3 +1,18 @@
 //  Math.random() lay ngau nhie:
 // Math.round() Lam tron ceil len floor xuong
 
+
+Solanmua=0;
+
+Somuonmua = -1;
+const somuaduoc = 93;
+
+// for(;Somuonmua != somuaduoc;){
+//     Somuonmua = Math.floor( Math.random()*100);
+//     console.log("Lan thu: " + ++Solanmua + "  Mua duoc: " + Somuonmua)
+// }
+
+while(Somuonmua != somuaduoc){
+    Somuonmua = Math.floor(Math.random()*100);
+    console.log(` lan mua  ${++Solanmua}  Mua duoc so  ${Somuonmua}`);
+}
