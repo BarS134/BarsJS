fcea66e88cfcae3e7101b79fa375f78019a6e12b
BarS134 <62279278+BarS134@users.noreply.github.com>
2020-08-08 22:37:30 +0700

    funtion js

---
+++ Funtiong.js
@@ -0,0 +1,33 @@
+// //  Funtion 
+// function Show(x){
+//     console.log(x);
+// }
+
+// Show();
+
+
+// function add(x,y){
+//     return x+y;   
+// }
+
+// function x(){
+
+// }
+
+// console.log(x());
+
+
+// const a = console.log;
+// a(100);
+
+// funtion 1 gia tri
+
+function getlog(){
+    return console.log;
+}
+
+// x = getlog();
+// x(1000);
+
+// or
+getlog()(100);
