ad91b6b4b691a6b3e2eb22ce93b1c6a67de218fe
BarS134 <62279278+BarS134@users.noreply.github.com>
2020-08-09 20:54:56 +0700

    Baitap1

---
+++ BaiTap.js
@@ -0,0 +1,13 @@
+/*
+    in ra các số  chia het cho 2 trong khoảng 0-100
+    in ra chí hết cho 3
+    in ra các số chia 5 dư 1
+    in ra số chính phương => math.qrt(x) %1;
+*/
+
+function soChiaHetCho2(){
+    for( i = 0; i< 100;i++)
+    if(i%2 == 0){
+        console.log(i);
+    }
+}
